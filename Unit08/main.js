b64e80fbf6f3ffd6ee48654cb427fea20ba7e414
khanhnet <khanhnet2632000@gmail.com>
2019-01-25 11:10:46 +0700

    HW10

--- Unit08/main.js
+++ Unit08/main.js
@@ -1,17 +1,17 @@
 
 
 
-
+/*
 document.getElementsByClassName('name')[1].style.color="red"
 document.getElementsByClassName('name')[0].style.backgroundColor="blue"
 document.getElementById('name').style.color="green"
 document.getElementById('click').addEventListener('click',
 	function(){
 		/*alert('hello')*/
-		document.getElementsByClassName('name')[1].style.color="blue"
-		document.getElementsByClassName('name')[0].style.backgroundColor="red"
+		/*document.getElementsByClassName('name')[1].style.color="blue"
+		document.getElementsByClassName('name')[0].style.backgroundColor="red"*/
 		/*document.getElementById('name').setAttribute('class','ahihi');*/
-		console.log(document.getElementById('name').getAttribute('class'))
+		/*console.log(document.getElementById('name').getAttribute('class'))
 
 })
 document.getElementById('mouse').addEventListener('mouseout',
@@ -27,7 +27,7 @@ document.getElementById('mouse').addEventListener('mouseover',
 		document.getElementsByClassName('name')[1].style.color="black"
 		document.getElementsByClassName('name')[0].innerHTML='ahihi'
 
-})
+})*/
 
 
 var input = document.getElementById('input')
@@ -35,16 +35,69 @@ var input = document.getElementById('input')
 document.getElementById('add').addEventListener('click',
 	function(){
 		var i=1
+		if (input.value=='') {
+			alert('error');
+		}else{
+			content.innerHTML +=    
+			`<tr id="`+i+`">
+			<td><input type="checkbox" ></td>
+			<td>`+input.value+`</td>
+			<td><button onclick = "funedit(`+i+`)">edit</button></td>
+			<td><button onclick = "fundel(`+i+`)">del</button></td>
+			</tr>`;
+			input.value=''
+		}
+
 		
-		/*alert(input.value)*/
-		content.innerHTML += /*input.value +'<br>'*/    
-		`<tr id="`+i+`">
-		<td>`+input.value+`</td>
-		<td><button onclick = "fundel(`+i+`)">del</button></td>
-		</tr>`;
-		input.value=''
+		
 })
 function fundel(id){
+	
+	if (confirm('do you want delete ?')==true) {
+		
 	document.getElementById(id).remove()
+	}
 }
+function funedit(id){
+	var input = document.getElementById('input')
+	input.value = input
+	document.getElementById('add').setAttribute(id, update)
+		document.getElementById('update').innerHTML="Update"
+	
+
+	
+}
+
+/*function check(status){
+	var checkboxs =document.getElementsByTagName('input');
+	 for (var i = 0; i < checkboxs.length; i++){
+                 checkboxs[i].checked = status
+                }
+
+}*/
+document.getElementById('checkall').addEventListener('click',
+	function(){
+		var checkboxs =document.getElementsByTagName('input');
+		 for (var i = 0; i < checkboxs.length; i++){
+                 checkboxs[i].checked = true;
+                }
+})
+document.getElementById('uncheckall').addEventListener('click',
+	function(){
+		var checkboxs =document.getElementsByTagName('input');
+		 for (var i = 0; i < checkboxs.length; i++){
+                      checkboxs[i].checked = false;
+                }
+                
+})
+document.getElementById('delall').addEventListener('click',
+	function(){
+		if (confirm('do you want delete all ?')==true) {
+		
+	content.innerHTML=''
+	}
+		
+		
+                
+})
 
