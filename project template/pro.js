e20cd981597753ba3b6768d60aefd1b7a169293c
khanhnet <khanhnet2632000@gmail.com>
2019-02-26 17:46:39 +0700

    pro

---
+++ project template/pro.js
@@ -0,0 +1,10 @@
+new WOW().init();
+
+ $(".slides").vegas({
+    slides: [
+        { src: "images/1 (1).jpg" },
+        { src: "images/1 (3).jpg" },
+        { src: "images/2 (1).jpg" },
+        { src: "images/3 (1).jpg" },
+    ]
+});
